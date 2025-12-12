24c2281ce35684b11006fe8dfb89269badf0bf08
SkyScube <103284841+SkyScube@users.noreply.github.com>
2025-12-12 11:39:52 +0100

    début auth
    * Login and decorator
    Login is ok
    Decorator bugging on line 20
    
    * Decorator fix
    
    * CHange encryption jwt
    
    * OUIIIIIIIIIIIIIIIIIIIIIII
    
    * Error better

---
+++ src/core/routes/auth/login.py
@@ -0,0 +1,53 @@
+import os
+from datetime import datetime, timedelta
+
+import jwt
+from flask import Blueprint, render_template, request, make_response, jsonify
+
+auth_blueprint = Blueprint("auth", __name__, url_prefix="/auth")
+
+@auth_blueprint.get("/login")
+def get_login():
+    return render_template("login.html")
+
+@auth_blueprint.post("/login")
+def post_login():
+    data = request.form.to_dict()
+    #user = get_user_by_email(data["email"])
+    #
+    # if not user:
+    #     return jsonify({
+    #         'message': 'Email or password incorrect',
+    #     }), 401
+    # if not verify_password(password, user.Password):
+    #     return jsonify({
+    #         'message': 'Email or password incorrect',
+    #     }), 401
+    #############################################################
+    # Temporaire
+    if data['email'] == "test@p5stock.fr" and data['password'] == "Sup€rP@ssw0rd":
+        access_payload = {
+            'user_id': "ububhbo",
+            'exp': datetime.utcnow() + timedelta(minutes=10),
+            'iat': datetime.utcnow(),
+            'type': 'access'
+        }
+        private_key = open('private.pem', 'rb').read()
+        access_token = jwt.encode(access_payload, private_key, algorithm='RS256')
+        response = make_response(jsonify({
+            'message': 'Login successful',
+        }), 200)
+        response.set_cookie(
+            'access_token',
+            access_token,
+            httponly=True,  # Prevent XSS
+            secure=True,  # HTTPS only (ANSSI required)
+            samesite='Strict',  # CSRF protection (ANSSI required)
+            max_age=10 * 60,  # 10 minutes
+            path='/'  # Explicit path
+        )
+        return response
+    return jsonify({
+             'message': 'Email or password incorrect',
+         }), 401
+    ################################################################
