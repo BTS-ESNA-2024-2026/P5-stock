884775237b7006d6ff7ee116019c58119ea2ce0c
true_rustyy <lili35lili35@gmail.com>
2026-01-13 17:13:07 +0100

    redirect auth

--- src/core/routes/root.py
+++ src/core/routes/root.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, send_from_directory
-from src.core.decorators.decorators import require_jwt, require_viewer, require_user
+from src.core.decorators.decorators import require_technician, require_viewer, require_user
 from loguru import logger
 
 base_blueprint = Blueprint("base", __name__)
@@ -14,6 +14,6 @@ def favicon():
     return send_from_directory('static', 'favicon.ico') # <-- soup
 
 @base_blueprint.route("/dashboard")
-#@require_jwt
+@require_technician
 def dash():
     return render_template("dashboard.html")
